0dbbf2837eee544f28eed698ba176f0dc93afe12
Junyi88 <the.junyi@gmail.com>
2017-09-01 11:51:10 +0100

    dbg

--- TakakiModel/src/TakPhase.cpp
+++ TakakiModel/src/TakPhase.cpp
@@ -2,7 +2,8 @@
 
 //##========================================================================
 // @@ -- Constructor ----------------------------------------------------
-ACBulk::ACBulk(JMpi inJMpi, const JMat &in_F) :
+template <class FDClass>
+ACBulk<class FDClass>::ACBulk(JMpi inJMpi, const JMat &in_F) :
   _MpiObj(inJMpi), _NY(_MpiObj.NYGl()), _NX(_MpiObj.NX()),
   _Ny(_MpiObj.NYLo()), _F(in_F), _F2(_NY,_NX), _F3(_NY,_NX), _F4(_NY,_NX),
   _D(_MpiObj, in_F) {}
