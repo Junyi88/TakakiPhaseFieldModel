cb78f2ec2fcdf3b12ee3abb322d882458d3712b6
Junyi88 <the.junyi@gmail.com>
2017-09-01 11:42:06 +0100

    Test TakPhase Template

---
+++ TakakiModel/inc/TakPhase.h
@@ -0,0 +1,32 @@
+#ifndef TAKPHASE_H
+#define TAKPHASE_H
+
+#include "JFDMpi.h"
+
+template <class FDClass>
+class ACBulk {
+public:
+  ACBulk(JMpi inJMpi, const JMat &in_F);
+  // ACBulk(JMpi inJMpi);
+	// ACBulk & operator= (const ACBulk &in1); //Write to operator
+  //
+  // //=========================
+  // // Useful Functions
+  // void SetF(const JMat &in_F);
+  // void Calc_All();
+  // void Calc_FD();
+  // void Calc_Powers();
+
+protected:
+  JMpi _MpiObj;
+	int _NY, _NX, _Ny;
+
+  JMat _F;
+  JMat _F2, _F3, _F4;
+  FDClass _D;
+
+  MPI_Status _status;
+};
+
+
+#endif
---
+++ TakakiModel/src/TakPhase.cpp
@@ -0,0 +1,8 @@
+#include "TakPhase.h"
+
+//##========================================================================
+// @@ -- Constructor ----------------------------------------------------
+ACBulk(JMpi inJMpi, const JMat &in_F) :
+  _MpiObj(inJMpi), _NY(_MpiObj.NYGl()), _NX(_MpiObj.NX()),
+  _Ny(_MpiObj.NYLo()), _F(in_F), _F2(_NY,_NX), _F3(_NY,_NX), _F4(_NY,_NX),
+  _D(_MpiObj, in_F) {}
