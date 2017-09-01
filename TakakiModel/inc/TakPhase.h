87b92d8cc6d4d8e9c731aabfe2bafaaa2fc44705
Junyi88 <the.junyi@gmail.com>
2017-09-01 13:14:37 +0100

    AddedGetValues

--- TakakiModel/inc/TakPhase.h
+++ TakakiModel/inc/TakPhase.h
@@ -20,6 +20,27 @@ public:
   void Calc_dP();
   void Calc_All();
 
+  //=========================
+  // GetPointers
+  JMat * FP(){return &_F;};
+  JMat * F2P(){return &_F2;};
+  JMat * PP(){return &_P;};
+  JMat * dPP(){return &_dP;};
+  FDClass * DP(){return &_D;};
+
+  // GetValues
+  double F(const int &y, const int &x){return _F.Value(j,i);};
+  double F2(const int &y, const int &x){return _F.Value(j,i);};
+  double P(const int &y, const int &x){return _P.Value(j,i);};
+  double dP(const int &y, const int &x){return _dP.Value(j,i);};
+
+  double Dx(const int &y, const int &x){return _D.Dx(j,i);};
+  double Dy(const int &y, const int &x){return _D.Dy(j,i);};
+  double Dxy(const int &y, const int &x){return _D.Dxy(j,i);};
+  double Dxx(const int &y, const int &x){return _D.Dxx(j,i);};
+  double Dyy(const int &y, const int &x){return _D.Dyy(j,i);};
+  double D2(const int &y, const int &x){return _D.D2(j,i);};
+
 protected:
   JMpi _MpiObj;
 	int _NY, _NX, _Ny;
