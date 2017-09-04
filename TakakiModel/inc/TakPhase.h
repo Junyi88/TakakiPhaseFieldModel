7d29441890526efd99fb753ce40e457136abb153
Junyi88 <the.junyi@gmail.com>
2017-09-04 16:13:48 +0100

    x

--- TakakiModel/inc/TakPhase.h
+++ TakakiModel/inc/TakPhase.h
@@ -14,8 +14,13 @@ protected:
 
 
   FDClass _F;
-  
+
 };
+//##========================================================================
+// @@ -- Constructor ----------------------------------------------------
+template <class FDClass>
+TakPhase<FDClass>::TakPhase(JMpi inJMpi,const JMat &in_F) :
+  _MpiObj(inJMpi), _F(_MpiObj.NYGl(),_MpiObj.NX()) {}
 
 
 #endif
