38b5b7f41dfc19d426270cfdd9f8e94f2491cd4e
Junyi88 <the.junyi@gmail.com>
2017-09-04 16:29:07 +0100

    Prepare Template System 1

--- TakakiModel/TestSrc/Test1.cpp
+++ TakakiModel/TestSrc/Test1.cpp
@@ -170,7 +170,7 @@ int main(int argc, char ** argv){
   LogFile << "Setup Phase Class " << std::endl;
 
   GClass<JMat> G(Eta0);
-  TakPhase<JFDMpi2DBase> Phi(MPIOBJ, Eta0);
+  TakPhase<JFDMpi2DReflectHigh> Phi(MPIOBJ, Eta0);
   // TakPhase<JMat> Phi(MPIOBJ, Eta0);
   LogFile << "Setup Phase Class Completed \n" << std::endl;
   //
