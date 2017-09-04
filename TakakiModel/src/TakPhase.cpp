8e2b883deac7fcbddffc67990ae70a675da5888b
Junyi88 <the.junyi@gmail.com>
2017-09-04 16:11:45 +0100

    xxx

--- TakakiModel/src/TakPhase.cpp
+++ TakakiModel/src/TakPhase.cpp
@@ -4,4 +4,4 @@
 // @@ -- Constructor ----------------------------------------------------
 template <class FDClass>
 TakPhase<FDClass>::TakPhase(JMpi inJMpi,const JMat &in_F) :
-  _MpiObj(inJMpi), _F(_NY,_NX) {}
+  _MpiObj(inJMpi), _F(_MpiObj.NYGl(),_MpiObj.NX()) {}
